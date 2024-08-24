526df343f27b8a10a92678d02fb56ea1f24e8157
boogabooga841 <kebabremover1234@outlook.com>
2024-08-24 13:02:09 +0300

    more sources

--- generate_channels.py
+++ generate_channels.py
@@ -35,7 +35,7 @@ def save_unique_channels(unique_channels, output_file):
 
 base_directory = 'sites'
 output_file = 'updated_channels.xml'
-target_dirs = ['tv.blue.ch','tvpassport.com']
+target_dirs = ['tv.blue.ch','tvpassport.com','tvguide.com','flixed.io']
 
 unique_channels = collect_unique_channels(base_directory, target_dirs)
 save_unique_channels(unique_channels, output_file)
